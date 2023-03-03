af6cffee73e45fffd7d8aea88dce54898a1baa94
Aron Phillimore <aron.phillimore@btinternet.com>
2023-03-03 17:02:56 +0000

    check format, lint and test

--- packages/jest-config/index.ts
+++ packages/jest-config/index.ts
@@ -1,12 +1,12 @@
 import { Config } from "jest";
 
 const config: Config = {
-  testEnvironment: "node",
-  preset: "ts-jest",
-  testPathIgnorePatterns: ["/node_modules/"],
-  transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest",
-  },
+    testEnvironment: "node",
+    preset: "ts-jest",
+    testPathIgnorePatterns: ["/node_modules/"],
+    transform: {
+        "^.+\\.(t|j)sx?$": "@swc/jest",
+    },
 };
 
 export default config;
--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,19 +1,19 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  tabWidth: 4,
-  useTabs: false,
-  semi: true,
-  printWidth: 120,
-  trailingComma: "all",
-  singleAttributePerLine: true,
-  singleQuote: false,
-  endOfLine: "lf",
-  overrides: [
-    {
-      files: ["*.ts", "*.mts", "*.tsx"],
-      options: {
-        parser: "typescript",
-      },
-    },
-  ],
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    printWidth: 120,
+    trailingComma: "all",
+    singleAttributePerLine: true,
+    singleQuote: false,
+    endOfLine: "lf",
+    overrides: [
+        {
+            files: ["*.ts", "*.mts", "*.tsx"],
+            options: {
+                parser: "typescript",
+            },
+        },
+    ],
 };
